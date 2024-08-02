1aed98e9d5871f56de0fea856d112b546abe63da
tison <wander4096@gmail.com>
2024-08-02 12:11:31 +0800

    feat: configure opendal
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ src/main/java/io/tisonkun/demo/springboot/opendal/OpenDALConfig.java
@@ -0,0 +1,39 @@
+package io.tisonkun.demo.springboot.opendal;
+
+import java.util.Map;
+import org.apache.opendal.AsyncOperator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenDALConfig {
+    @Value("${opendal.scheme}")
+    private String scheme;
+
+    @Value("${opendal.endpoint}")
+    private String endpoint;
+
+    @Value("${opendal.bucket}")
+    private String bucket;
+
+    @Value("${opendal.region}")
+    private String region;
+
+    @Value("${opendal.access_key_id}")
+    private String accessKeyId;
+
+    @Value("${opendal.secret_access_key}")
+    private String secretAccessKey;
+
+    @Bean
+    public AsyncOperator opendal() {
+        return AsyncOperator.of(scheme, Map.of(
+            "endpoint", endpoint,
+            "bucket", bucket,
+            "region", region,
+            "access_key_id", accessKeyId,
+            "secret_access_key", secretAccessKey
+        ));
+    }
+}
